65efa27cd3626de0819cd05947706826b6df8aaf
EricKossowski <ericvgamer@gmail.com>
2018-05-25 13:31:44 -0400

    Sound
    Some music

---
+++ Assets/_SRC/Scripts/Utilities/buttonAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAudio : MonoBehaviour {
+
+    AudioSource soundFX;
+    public AudioClip[] soundByte;
+
+    public void PlaySound(int a)
+    {
+        soundFX.clip = soundByte[a];
+        soundFX.Play();
+    }
+
+    // Use this for initialization
+    void Start () {
+        soundFX = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
